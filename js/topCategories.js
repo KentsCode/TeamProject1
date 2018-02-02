196d7f0e31645992c15653b8dce6ae99ea299bd1
KentsCode <kentmbox42@yahoo.com>
2018-02-01 18:36:01 -0600

    Updated topcategories.js

---
+++ js/topCategories.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+//sets local storage item to the category selected
+	$(".category").on("click", function() {
+		console.log($(this).attr("id"));
+		localStorage.setItem("lastSearch", $(this).attr("id"));
+		console.log(localStorage.getItem("lastSearch"));
+	});
+
+});
